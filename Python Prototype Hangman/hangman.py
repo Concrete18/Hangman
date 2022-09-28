5c4524c72b07a335c63f44d1c32c8462d6dcef7a
Concrete18 <michaelericson19@gmail.com>
2022-09-28 18:39:53 -0400

    finished fully working code except for word repeat bug

--- Python Prototype Hangman/hangman.py
+++ Python Prototype Hangman/hangman.py
@@ -1,13 +1,28 @@
-import random
+import random, os, time, json
+
+
+def keyboard_interrupt(func):
+    """
+    Catches all KeyboardInterrupt exceptions.
+    Closes with a message and delayed program exit.
+    """
+
+    def wrapped(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except KeyboardInterrupt:
+            delay = 0.1
+            print(f"\nClosing in {delay} second(s)")
+            time.sleep(delay)
+            exit()
+
+    return wrapped
 
 
 class Hangman:
     """
     ph
     """
-
-    word_list = ["Wolverine", "Iron Man", "Dead Pool"]
-    current_word = ""
 
     def __init__(self) -> None:
         """
@@ -19,70 +34,147 @@ class Hangman:
         """
         ph
         """
-        if self.word_list:
-            new_word = random.choice(self.word_list)
-            self.word_list.pop(self.word_list.index(new_word))
+        if self.words_list:
+            new_word = random.choice(self.words_list)
+            self.words_list.pop(self.words_list.index(new_word))
             self.current_word = new_word
             return self.current_word
         else:
             return False
 
-    def display_stick_man(self, removed_limbs=0, left_padding=4):
+    def load_words_list(self):
+        """
+        ph
+        """
+        with open("words_list.json") as file:
+            self.words_list = json.load(file)
+
+    def display_stick_man(self, parts=0, left_padding=9):
         """
         ph
         """
         stick_man = [
             ["|", "-", "-", "-", "-", "|", " ", " ", " ", " "],
             ["|", " ", " ", " ", " ", "|", " ", " ", " ", " "],
-            ["|", " ", " ", " ", " ", "O", " ", " ", " ", " "],
-            ["|", " ", " ", " ", "/", "|", "\\", " ", " ", " "],
-            ["|", " ", " ", " ", "/", " ", "\\", " ", " ", " "],
+            ["|", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ["|", " ", " ", " ", " ", " ", " ", " ", " ", " "],
+            ["|", " ", " ", " ", " ", " ", " ", " ", " ", " "],
             ["|", "_", "_", "_", "_", "_", "_", "_", "_", "_"],
         ]
-        remove_order = [
-            (4, 6),  # right leg
-            (4, 4),  # left leg
-            (3, 6),  # right arn
-            (3, 4),  # left arm
-            (3, 5),  # body
-            (2, 5),  # head
+        add_order = [
+            (2, 5, "0"),  # head
+            (3, 5, "|"),  # body
+            (3, 4, "/"),  # left arm
+            (3, 6, "\\"),  # right arn
+            (4, 4, "/"),  # left leg
+            (4, 6, "\\"),  # right leg
         ]
-        for num in range(removed_limbs):
-            x, y = remove_order[num]
-            stick_man[x][y] = " "
+        for num in range(parts):
+            x, y, part = add_order[num]
+            stick_man[x][y] = part
+        print()
         for line in stick_man:
             padding = " " * left_padding
             print(f'{padding}{"".join(line)}')
 
-    def print_hidden_word(self, word, known_chars: list = []):
+    def print_hidden_word(self, word, known_letters: list, left_padding=4):
         """
         ph
         """
         final_string_list = []
-        known_chars.append(" ")
+        missing_count = 0
         for char in word:
-            if char.lower() in known_chars:
+            if char.lower() in known_letters:
                 final_string_list.append(char)
             else:
                 final_string_list.append("_")
+                missing_count += 1
         final_string = " ".join(final_string_list)
-        print(final_string)
+        print(" " * left_padding, final_string)
+        return not missing_count
 
+    def play_again(self):
+        """
+        ph
+        """
+        response = input("\nDo you want to play again?\n")
+        if response.lower() in ["y", "yes"]:
+            self.play()
+        else:
+            print("Thanks for playing!")
+            exit()
+
+    def guess(self):
+        """
+        ph
+        """
+        self.error = None
+        guess = input("\nType a letter or a full guess:\n")
+        # full guess
+        if guess.lower() == self.current_word.lower():
+            print("\nYou win!")
+            self.play_again()
+        # letter guess
+        elif len(guess) == 1:
+            guess = guess.lower()
+            # TODO fix issue with extra space before errors
+            if guess in self.known_letters:
+                self.error = "\nYou already guessed that correctly."
+                return
+            elif guess in self.current_word.lower():
+                self.known_letters.append(guess)
+                return
+            elif guess in self.incorrect_guess:
+                self.error = "\nYou already guessed that incorrectly."
+                return
+        self.incorrect_guess.append(guess)
+        self.losses += 1
+        print("\nIncorrect")
+        if self.losses == 6:
+            self.display_stick_man(6)
+            print("\nYou lose!")
+            self.play_again()
+
+    @keyboard_interrupt
     def play(self):
         """
         ph
         """
-        print("Welcome to the game of Hangman")
+        # setup
+        self.load_words_list()
+        self.current_word = None
+        self.known_letters = [" "]
+        self.incorrect_guess = []
+        self.error = None
+        self.losses = 0
+
         # picks current word
-        if not self.get_new_word():
-            msg = "You win! There are no more words left."
-            print(msg)
-            return
-        self.print_hidden_word(self.current_word)
+        self.get_new_word()
+        while self.losses < 6:
+            os.system("cls" if os.name == "nt" else "clear")
+            print("Welcome to the game of Hangman\n")
+            win = self.print_hidden_word(
+                self.current_word,
+                self.known_letters,
+            )
+            if win:
+                print("\nYou win!")
+                self.play_again()
+            self.display_stick_man(self.losses)
+            # show incorrect guesses
+            if self.incorrect_guess:
+                wrong_guesses = ", ".join(self.incorrect_guess)
+                print(f"\nWrong Guesses: {wrong_guesses}")
+            # error
+            if self.error:
+                print(self.error)
+            # guess the word or letter
+            self.guess()
+        msg = "\nYou win!\nThere are no more words left."
+        print(msg)
+        return
 
 
 if __name__ == "__main__":
     App = Hangman()
     App.play()
-    # App.print_hidden_word("Iron Man", ["t", "a", "e", "i"])
-    # App.display_stick_man(4)
