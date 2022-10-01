547edb55ad37df0b86cf2cd2582e6a3e767d055c
Concrete18 <michaelericson19@gmail.com>
2022-10-01 15:35:42 -0400

    fixed some bugs and turned some code into a reusable function

--- Rust Hangman/src/main.rs
+++ Rust Hangman/src/main.rs
@@ -1,21 +1,43 @@
-fn print_hidden_word(word: &str, known_letters: Vec<char>) -> bool {
-    let mut final_string_vec: Vec<char> = Vec::new();
-    let mut missing_count: u8 = 0;
-    for c in word.chars() {
-        // set c to lower case .to_lowercase()
-        if known_letters.contains(&c) {
-            final_string_vec.push(c);
-        } else {
-            final_string_vec.push('_');
-            missing_count = &missing_count + 1;
-        }
-    }
-    let final_string = final_string_vec
+// def get_new_word(self):
+//     """
+//     Gets a new word for playing
+//     """
+//     if self.words_list:
+//         new_word = random.choice(self.words_list)
+//         self.words_list.pop(self.words_list.index(new_word))
+//         self.current_word = new_word
+//         return self.current_word
+//     else:
+//         return False
+
+// use rand::seq::SliceRandom; // 0.7.2
+
+// fn get_new_word(words_list: &[String]) {
+//     let new_word: String = words_list.choose(&mut rand::thread_rng());
+// }
+
+fn join_vector(vec: Vec<char>) -> String {
+    return vec
         .iter()
         .map(|e| e.to_string())
         .collect::<Vec<String>>()
         .join(" ");
+}
+
+fn print_hidden_word(word: &str, known_letters: Vec<char>) -> bool {
+    let mut final_string_vec: Vec<char> = Vec::new();
+    let mut missing_count: u8 = 0;
+    for c in word.chars() {
+        if known_letters.contains(&c.to_ascii_lowercase()) {
+            final_string_vec.push(c);
+        } else {
+            final_string_vec.push('_');
+            missing_count = missing_count + 1;
+        }
+    }
+    let final_string: String = join_vector(final_string_vec);
     println!("{}", final_string);
+    // returns true if no more underscores
     return missing_count == 0;
 }
 
