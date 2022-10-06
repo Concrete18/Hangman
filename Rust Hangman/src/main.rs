2f7c0927a4bfaac7547f9fd1ab27faea45261a15
Concrete18 <michaelericson19@gmail.com>
2022-10-05 23:08:53 -0400

    fixed many bugs with ownership and nearly finished the app

--- Rust Hangman/src/main.rs
+++ Rust Hangman/src/main.rs
@@ -1,9 +1,31 @@
 use rand::Rng;
+// use std::fs;
 use std::io;
+
+/// loads words list from text file
+fn load_words_list() -> Vec<String> {
+    let words_list = vec![
+        "Rust".to_string(),
+        "Linux".to_string(),
+        "Programming".to_string(),
+        "Virtual Reality".to_string(),
+        "Algorithm".to_string(),
+        "Arrays".to_string(),
+        "Binary".to_string(),
+        "Computer".to_string(),
+        "Terminal".to_string(),
+    ];
+
+    // let file_path = "words_list.txt";
+    // let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    // let words_list = contents.trim().split('\n').collect::<Vec<String>>();
+
+    words_list
+}
 
 /// asks for input after printing a msg
 fn get_input(msg: &str) -> String {
-    println!("{}", msg);
+    println!("{msg}");
     let mut response = String::new();
     io::stdin()
         .read_line(&mut response)
@@ -47,25 +69,25 @@ fn join_vector(vec: Vec<char>, sep: String) -> String {
 fn display_stick_man(parts: u8) {
     match parts {
         0 => println!(
-            "\n    |-------|\n    |       |\n    |        \n    |      \n    |      \n    |________________"
+            "\n    |-------|\n    |       |\n    |        \n    |      \n    |      \n    |        \n    |________________"
         ),
         1 => println!(
-            "\n    |-------|\n    |       |\n    |       O\n    |      \n    |      \n    |________________"
+            "\n    |-------|\n    |       |\n    |       O\n    |      \n    |      \n    |        \n    |________________"
         ),
         2 => println!(
-            "\n    |-------|\n    |       |\n    |       O\n    |       |  \n    |      \n    |________________"
+            "\n    |-------|\n    |       |\n    |       O\n    |       |  \n    |      \n    |        \n    |________________"
         ),
         3 => println!(
-            "\n    |-------|\n    |       |\n    |       O\n    |      /| \n    |      \n    |________________"
+            "\n    |-------|\n    |       |\n    |       O\n    |      /| \n    |      \n    |        \n    |________________"
         ),
         4 => println!(
-            "\n    |-------|\n    |       |\n    |       O\n    |      /|\\\n    |      \n    |________________"
+            "\n    |-------|\n    |       |\n    |       O\n    |      /|\\\n    |      \n    |        \n    |________________"
         ),
         5 => println!(
-            "\n    |-------|\n    |       |\n    |       O\n    |      /|\\\n    |      /  \n    |________________"
+            "\n    |-------|\n    |       |\n    |       O\n    |      /|\\\n    |      /  \n    |        \n    |________________"
         ),
         _ => println!(
-            "\n    |-------|\n    |       |\n    |       O\n    |      /|\\\n    |      / \\\n    |________________"
+            "\n    |-------|\n    |       |\n    |       O\n    |      /|\\\n    |      / \\\n    |        \n    |________________"
         ),
     }
 }
@@ -83,68 +105,71 @@ fn print_hidden_word(hidden_word: &String, known_letters: &Vec<char>) -> (bool, 
         }
     }
     let final_string: String = join_vector(final_string_vec, " ".to_string());
-    println!("{}", final_string);
+    println!("{final_string}");
     // returns true if no more underscores and the printed string
-    return (missing_count == 0, final_string);
+    (missing_count == 0, final_string)
 }
 
-fn guess(hidden_word: String, known_letters: Vec<char>, incorrect_guesses: &Vec<String>) {
-    // TODO finish function
-    let mut guess = get_input("\nType a letter or a full guess:\n");
-    guess = guess.to_lowercase();
-    // full guess
-    if guess == hidden_word.to_lowercase() {
-        println!("\nYou win!");
-        play_again()
-    // letter guess
-    } else if guess.len() == 1 {
-        // guessed letter was already chosen
-        let guess_char = guess.chars().next().expect("string is empty");
-        if known_letters.contains(&guess_char) {}
-        // guessed letter or word was already used incorrectly
-        if incorrect_guesses.contains(&guess) {}
-        // guessed letter is in the current word
-        if hidden_word.contains(&guess) {}
-    // blank response causes a new prompt for a guess again
-    } else if guess == "" {
-        // TODO update error to "\nPlease type in a valid guess."
-    }
-}
-
-fn main() {
-    let mut words_list = vec![
-        "Rust".to_string(),
-        "Linux".to_string(),
-        "Programming".to_string(),
-        "Virtual Reality".to_string(),
-        "Algorithm".to_string(),
-        "Arrays".to_string(),
-        "Binary".to_string(),
-        "Computer".to_string(),
-        "Terminal".to_string(),
-    ];
-
+fn play(mut words_list: Vec<String>) {
     let hidden_word = words_list.take_random_item();
     let mut known_letters: Vec<char> = vec![' '];
     let mut incorrect_guesses: Vec<String> = Vec::new();
     let mut losses: u8 = 0;
+    let mut error = String::new();
     while losses < 6 {
         // TODO clear terminal
+        termion::clear::All;
         println!("Welcome to the game of Hangman\n");
         let win = print_hidden_word(&hidden_word, &known_letters).0;
         display_stick_man(losses);
         if win {
             println!("\nYou Win!");
-            play_again()
+            play_again();
+            return;
         }
         if incorrect_guesses.len() > 0 {
             let wrong_guesses = incorrect_guesses.join(", ");
-            println!("Wrong Guesses: {}", wrong_guesses);
+            println!("\nWrong Guesses: {wrong_guesses}");
         }
-        // TODO show errors
-        guess(hidden_word, known_letters, &incorrect_guesses)
+        // show errors if they exist
+        println!("{error}");
+        // gets guess
+        let mut guess = get_input("\nType a letter or a full guess:\n");
+        guess = guess.to_lowercase();
+        // full guess
+        if guess == hidden_word.to_lowercase() {
+            println!("\nYou win!");
+            play_again();
+            return;
+        // letter guess
+        } else if guess.len() == 1 {
+            // guessed letter was already chosen
+            let guess_char = guess.chars().next().expect("string is empty");
+            if known_letters.contains(&guess_char) {
+                error = "\nYou already guessed that correctly.".to_string();
+            }
+            // guessed letter or word was already used incorrectly
+            else if incorrect_guesses.contains(&guess) {
+                error = "\nYou already guessed that incorrectly.".to_string();
+            } else if hidden_word.contains(&guess) {
+                // guessed letter is in the current word
+                known_letters.push(guess_char);
+            }
+        // blank response causes a new prompt for a guess again
+        } else if guess == "" {
+            error = "\nPlease type in a valid guess.".to_string();
+        } else {
+            incorrect_guesses.push(guess);
+            losses = losses + 1;
+        }
     }
-    // TODO display win text if no words are left
+    get_input("\nYou win!\nThere are no more words left.");
+}
+
+fn main() {
+    let words_list = load_words_list();
+
+    play(words_list)
 }
 
 #[cfg(test)]
