b5dc724de836bdaaf9355c91be40b26f58b7e36a
Concrete18 <michaelericson19@gmail.com>
2022-10-07 01:16:44 -0400

    improved misc code readability

--- Rust Hangman/src/main.rs
+++ Rust Hangman/src/main.rs
@@ -1,26 +1,16 @@
+#![allow(warnings, unused)]
 use clearscreen::ClearScreen;
 use rand::Rng;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::{fs, io};
 
 /// loads words list from text file
-fn load_words_list() -> Vec<String> {
-    let words_list = vec![
-        "Rust".to_string(),
-        "Linux".to_string(),
-        "Programming".to_string(),
-        "Virtual Reality".to_string(),
-        "Algorithm".to_string(),
-        "Arrays".to_string(),
-        "Binary".to_string(),
-        "Computer".to_string(),
-        "Terminal".to_string(),
-    ];
-
-    // let file_path = "words_list.txt";
-    // let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-    // let words_list = contents.trim().split('\n').collect::<Vec<String>>();
-
-    words_list
+fn read_lines(path: &str) -> Result<Vec<String>, std::io::Error> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let lines: Vec<String> = reader.lines().collect::<Result<_, _>>()?;
+    Ok(lines)
 }
 
 /// asks for input after printing a msg
@@ -67,48 +57,32 @@ fn join_vector(vec: Vec<char>, sep: String) -> String {
 }
 
 /// Displays stick man with n parts shown
-fn display_stick_man(parts: u8) {
+fn get_stickman(parts: u8) -> String {
     match parts {
-        0 => println!(
-            "\n    |-------|\n    |       |\n    |        \n    |      \n    |      \n    |        \n    |________________"
-        ),
-        1 => println!(
-            "\n    |-------|\n    |       |\n    |       O\n    |      \n    |      \n    |        \n    |________________"
-        ),
-        2 => println!(
-            "\n    |-------|\n    |       |\n    |       O\n    |       |  \n    |      \n    |        \n    |________________"
-        ),
-        3 => println!(
-            "\n    |-------|\n    |       |\n    |       O\n    |      /| \n    |      \n    |        \n    |________________"
-        ),
-        4 => println!(
-            "\n    |-------|\n    |       |\n    |       O\n    |      /|\\\n    |      \n    |        \n    |________________"
-        ),
-        5 => println!(
-            "\n    |-------|\n    |       |\n    |       O\n    |      /|\\\n    |      /  \n    |        \n    |________________"
-        ),
-        _ => println!(
-            "\n    |-------|\n    |       |\n    |       O\n    |      /|\\\n    |      / \\\n    |        \n    |________________"
-        ),
+        0 => return "\n    |-------|\n    |       |\n    |        \n    |      \n    |      \n    |        \n    |________________".to_string(),
+        1 => return "\n    |-------|\n    |       |\n    |       O\n    |      \n    |      \n    |        \n    |________________".to_string(),
+        2 => return "\n    |-------|\n    |       |\n    |       O\n    |       |  \n    |      \n    |        \n    |________________".to_string(),
+        3 => return "\n    |-------|\n    |       |\n    |       O\n    |      /| \n    |      \n    |        \n    |________________".to_string(),
+        4 => return "\n    |-------|\n    |       |\n    |       O\n    |      /|\\\n    |      \n    |        \n    |________________".to_string(),
+        5 => return "\n    |-------|\n    |       |\n    |       O\n    |      /|\\\n    |      /  \n    |        \n    |________________".to_string(),
+        _ => return "\n    |-------|\n    |       |\n    |       O\n    |      /|\\\n    |      / \\\n    |        \n    |________________".to_string(),
     }
 }
 
 /// Prints `hidden_word` with all letters not in `known_letters` replaced with _
-fn print_hidden_word(hidden_word: &String, known_letters: &Vec<char>) -> (bool, String) {
-    let mut final_string_vec: Vec<char> = Vec::new();
+fn censor_hidden_word(hidden_word: &String, known_letters: &Vec<char>) -> (String, bool) {
+    let mut censored_string_vec: Vec<char> = Vec::new();
     let mut missing_count: u8 = 0;
     for c in hidden_word.chars() {
         if known_letters.contains(&c.to_ascii_lowercase()) {
-            final_string_vec.push(c);
+            censored_string_vec.push(c);
         } else {
-            final_string_vec.push('_');
+            censored_string_vec.push('_');
             missing_count = missing_count + 1;
         }
     }
-    let final_string: String = join_vector(final_string_vec, " ".to_string());
-    println!("{final_string}");
-    // returns true if no more underscores and the printed string
-    (missing_count == 0, final_string)
+    let censored_word: String = join_vector(censored_string_vec, " ".to_string());
+    (censored_word, missing_count == 0)
 }
 
 fn play(mut words_list: Vec<String>) {
@@ -124,26 +98,32 @@ fn play(mut words_list: Vec<String>) {
     let mut error = String::new();
     while losses < 6 {
         // clears terminal before each rewrite
-        ClearScreen::default()
-            .clear()
-            .expect("failed to clear the screen");
+        ClearScreen::default().clear().expect("Clear Failed");
         // new game rewrite of for each loop
         println!("Welcome to the game of Hangman\n");
-        let win = print_hidden_word(&hidden_word, &known_letters).0;
-        display_stick_man(losses);
+        // censores hidden word
+        let (censored_word, win) = censor_hidden_word(&hidden_word, &known_letters);
+        println!("{censored_word}");
+        // prints stickman
+        let stickman = get_stickman(losses);
+        println!("{stickman}");
+        // checks if user has won
         if win {
             println!("\nYou Win!");
             play_again(words_list);
             return;
         }
+        // shows all incorrect guesses if any exist
         if incorrect_guesses.len() > 0 {
             let wrong_guesses = incorrect_guesses.join(", ");
-            println!("\nWrong Guesses: {wrong_guesses}");
+            println!("\nIncorrect Guesses: {wrong_guesses}");
         }
         // show errors if they exist
-        println!("{error}");
+        if error.len() > 0 {
+            println!("{error}");
+        }
         // gets guess5
-        println!("Type a letter or a full guess:");
+        println!("\nType a letter or a full guess:");
         let mut guess = input();
         guess = guess.to_lowercase();
         // full guess
@@ -170,7 +150,9 @@ fn play(mut words_list: Vec<String>) {
             } else {
                 incorrect_guesses.push(guess.trim().to_string());
                 losses = losses + 1;
-                display_stick_man(losses);
+                // TODO fix incorrect reset after loss
+                let stickman = get_stickman(losses);
+                println!("{stickman}");
             }
         }
     }
@@ -179,7 +161,20 @@ fn play(mut words_list: Vec<String>) {
 }
 
 fn main() {
-    let words_list = load_words_list();
+    // let file_path = "words_list.txt";
+    // let words_list = load_words_list(file_path)?;
+
+    let words_list = vec![
+        "Rust".to_string(),
+        "Linux".to_string(),
+        "Programming".to_string(),
+        "Virtual Reality".to_string(),
+        "Algorithm".to_string(),
+        "Arrays".to_string(),
+        "Binary".to_string(),
+        "Computer".to_string(),
+        "Terminal".to_string(),
+    ];
 
     play(words_list)
 }
@@ -199,29 +194,32 @@ mod hangman_tests {
         }
     }
 
-    // TODO test empty words_list
+    #[test]
+    fn take_random_item_empties_vector() {
+        let mut words_list = vec!["Rust".to_string(), "Linux".to_string()];
+        for _ in 0..2 {
+            words_list.take_random_item();
+        }
+        assert_eq!(words_list.len(), 0);
+    }
 
     #[test]
     fn hidden_word_printing_no_win() {
         let known_letters: Vec<char> = vec!['t', 'e'];
-        let result = print_hidden_word(&"This is a test".to_string(), &known_letters);
-        let answer = false;
-        assert_eq!(result.0, answer);
+        let (string, win) = censor_hidden_word(&"Test".to_string(), &known_letters);
+        // tests string
+        assert_eq!(string, "T e _ t".to_string());
+        // tests win
+        assert_eq!(win, false);
     }
 
     #[test]
     fn hidden_word_printing_win() {
         let known_letters: Vec<char> = vec!['t', 'e', 's'];
-        let result = print_hidden_word(&"test".to_string(), &known_letters);
-        let answer = true;
-        assert_eq!(result.0, answer);
-    }
-
-    #[test]
-    fn hidden_word_printing_value() {
-        let known_letters: Vec<char> = vec!['t', 'e'];
-        let result = print_hidden_word(&"This is a test".to_string(), &known_letters);
-        let answer = "T _ _ _ _ _ _ _ _ _ t e _ t".to_string();
-        assert_eq!(result.1, answer);
+        let (string, win) = censor_hidden_word(&"Test".to_string(), &known_letters);
+        // tests string
+        assert_eq!(string, "T e s t".to_string());
+        // tests win
+        assert_eq!(win, true);
     }
 }
